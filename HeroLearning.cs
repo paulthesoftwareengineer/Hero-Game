36113ccf7ef926ff42bcbcbfce2303753f10003c
Paul Turner <102317855+paulthesoftwareengineer@users.noreply.github.com>
2022-08-15 18:36:46 -0700

    Create HeroLearning.cs

---
+++ HeroLearning.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    public string characterClass = "Marine";
+    private int currentAge = 19;
+    public int addedAge =  1;
+
+    public float cloud = 10f;
+    public string clanName = "Warrior";
+    public bool isMarine = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ComputeAge();
+        int characterHealth = 100;
+        Debug.Log(characterClass + " - HP:" + characterHealth);
+        Debug.Log($"The health of {clanName} is {characterHealth}!");
+
+    }
+
+    void CreateCharacter()
+    {
+        int characterName = "Warrior";
+        Debug.Log(characterName + "-" + characterClass);
+    }
+
+    // Update is called once per frame
+    void ComputeAge()
+    {
+        Debug.Log(currentAge + addedAge);
+    }
+}
